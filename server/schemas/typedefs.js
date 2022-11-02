63b8b1c519882a5f6804743e78fe12392c3f69e2
devihall <devis0306@gmail.com>
2022-11-02 13:49:36 -0400

    update auth.js

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -26,6 +26,8 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User
+    removeBook(bookId: String!): User
   }
 
   input bookInput {
@@ -43,8 +45,7 @@ const typeDefs = gql`
   }
 `; // ABOVE-type Query- users will return an array of ALL the users data type
 // user query- enforces a query parameter-exclamation point ! after the query parameter data type definitions indicates that for that query to be carried out, that data must exist, otherwise throw error
-// thoughts will return an array of ALL the Thought data type
-//single thought query -enforces a query parameter-returns an object of the Thought data type
+
 
 /////ABOVE-type Mutation- a login() mutation and an addUser() mutation. Both will return a User object: either the user who successfully logged in or the user who was just created on sign-up.-enforces a query parameter-a user can't be created without a username, email, and password.
 // authentication to other mutations to know which user is creating a new thought, reacting to an existing thought, or adding a friend.
